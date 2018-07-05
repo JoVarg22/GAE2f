ba5181110d5ea5c243a93a29ce11a8522755bfeb
JoVarg22 <39599709+JoVarg22@users.noreply.github.com>
2018-07-04 18:07:42 -0600

    Add files via upload
    Configuracion de la conexion WIFI y los pines del Arduino Adafruit

---
+++ sketch_jun28b.ino
@@ -0,0 +1,74 @@
+#include <ESP8266WiFi.h>
+ 
+const char* ssid     = "jovarg22";
+const char* password = "jonathan";
+ 
+const char* host = "172.20.10.6";
+int inPin = 14;
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+ 
+  // We start by connecting to a WiFi network
+ 
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  pinMode(inPin, INPUT);
+  
+}
+ 
+void loop() {
+  int val = 0;
+  String text = "LOW";
+  delay(5000);
+ 
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int port = 4444;
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  //String url = "/testwifi/index.html";
+  //Serial.print("Requesting URL: ");
+ // Serial.println(url);
+  
+  // This will send the request to the server
+  for(int x = 0; x < 10; x++){
+    
+    val = digitalRead(inPin);
+    if(val > 0){
+      text = "HIGH";
+    }else{
+      text = "LOW";
+    }
+    client.println(text);
+    delay(1200);
+  }
+  
+  // Read all the lines of the reply from server and print them to Serial
+  //while(client.available()){
+    //String line = client.readStringUntil('\r');
+    //Serial.print(line);
+  //}
+  
+  //Serial.println();
+  //Serial.println("closing connection");
+}
